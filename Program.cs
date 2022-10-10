efae6a4ca9b228e151ec275a44b690d7102d5068
Gayane Tsaturyan <gayane.tsaturyan.2020@gmail.ru>
2022-10-10 21:14:59 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using NewString2;
+
+Console.Write("Input sentence : ");
+string sentence = Console.ReadLine();
+SringFunctions myFunction = new SringFunctions();
+int sentenceLength = myFunction.ReturnWords(sentence).Length;
+for (int i = 0; i < sentenceLength; i++)
+{
+    Console.Write(myFunction.MyReverse(myFunction.ReturnWords(sentence)[i]) + " ");
+}
---
+++ SringFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace NewString2
+{
+    internal class SringFunctions
+    {
+        public string MyReverse(string txt)
+        {
+            var charTxt = txt.ToCharArray();
+            StringBuilder sb = new();
+
+            for (int i = txt.Length - 1; i >= 0; i--)
+                sb.Append(charTxt[i]);
+            return sb.ToString();
+        }
+        public string[] ReturnWords(string? sentence)
+        {
+            string[] def= { " Null "};
+            if (!String.IsNullOrEmpty(sentence) || !String.IsNullOrWhiteSpace(sentence))
+            {
+                string[] words = sentence.Split(' ');
+                return words;   
+            }
+            return def;
+        }
+
+    }
+}
